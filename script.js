cff1523126db15f65ffada7be14fead81968883a
your-m1nd <vi.neurogirl@gmail.com>
2023-07-14 19:48:18 +0300

    add script

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-let inputText = document.getElementById('text');
-let ulList = document.getElementById('list');
+let inputText = document.getElementById('typeText');
+let ulList = document.getElementById('ulList');
+let send = document.getElementById('send');
+let clear = document.getElementById('clear');
 
-function createTask() {
-    if (inputText.value === '') {
-        alert('Напиши задачу');
-    } 
-    else {
-        let li = document.createElement('li');
-        li.textContent = inputText.value;
-        ulList.appendChild('li');
-    }
+//создать список задач
+send.onclick = function createTask(evt) {
+if (inputText.value === '') {
+  let span = document.getElementById('warning');
+  span.textContent = 'Задачи отсутствуют'
+  clear.disabled = 'true';
+} else {
+  let li = document.createElement('li');
+  let task = typeText.value;
+  li.textContent = task;
+  ulList.appendChild(li);
+
+  //сохранить список в LocalStorage
+
+  //добавить пробел и ; в отображении
+  localStorage.setItem('task', `${ulList.textContent};`);
+
+  clear.style = 'background-color: orange';
+}
 }
 
-//function saveTasks() {
-  //  localStorage.setItem('task', ulList.innerHTML);
-//};
-
-//function showTask() {
-  // ulList.innerHTML = localStorage.getItem('task');
-//};
-
-//showTask();
+clear.onclick = function clearTasks(evt) {
+  ulList.removeChild(li);
+  localStorage.removeItem('task');
+  
+}
